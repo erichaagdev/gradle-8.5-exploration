3eecb5153e3239d7eda7e19807a024c66fdb44bc
Eric Haag <ehaag@gradle.com>
2024-01-08 15:59:58 -0600

    Add improvements to javadoc version catalogs example

---
+++ 06-improvements-to-javadoc-version-catalogs/06-gradle-8.5/settings.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.gradle.enterprise") version "3.16.1"
+}
+
+rootProject.name = "06-gradle-8.5"
+
+gradleEnterprise {
+    server = "https://ge.solutions-team.gradle.com"
+
+    buildScan {
+        publishAlways()
+    }
+}
